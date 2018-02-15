26e7f80bbe7a444ed8ba876873b4371cb463321f
Simon Lee <jaewon.lee@mail.mcgill.ca>
2018-02-15 10:47:45 -0500

    Fixed version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './structure';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {
     BrowserRouter as Router,
@@ -10,11 +10,32 @@ import {
     Switch
 } from 'react-router-dom';
 
+//bodies
+import MainPage from './bodies/main'
+import NavigationBar from './navbar/navigationBar'
+import StudentLanding from './bodies/studentLanding'
+import ProfLanding from './bodies/professorLanding'
+import AdminLanding from './bodies/adminLanding'
 
+const Redirection=()=>(
+    <Redirect to={"/main"}/>
+);
 
 //<Route path="/app/*" component={MainPage}/>
 
 ReactDOM.render(
-    <App/>
+    <div>
+        <NavigationBar/>
+        <Router>
+            <Switch>
+                <Route exact path="/main" component={MainPage} />
+                <Route exact path="/student/*" component={StudentLanding} />
+                <Route exact path="/prof/*" component={ProfLanding}/>
+                <Route exact path="/admin/*" component={AdminLanding}/>
+                <Route path="/*" component={Redirection} />
+            </Switch>
+        </Router>
+
+    </div>
     , document.getElementById('root'));
 registerServiceWorker();
