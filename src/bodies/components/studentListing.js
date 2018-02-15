ce638be1081210d3a3f7e478ebc394401dab41f0
durham-abric <durham.abric@mail.mcgill.ca>
2018-02-14 21:15:09 -0500

    fix Node Modules

--- src/bodies/components/studentListing.js
+++ src/bodies/components/studentListing.js
@@ -29,8 +29,11 @@ class StudentListing extends Component {
               <Col lg={6} md={6} sm={6} xs={6} className='center' style={{height:'100%',fontSize:15}}>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt congue ligula in rutrum. Morbi nec lacus condimentum, hendrerit mi eu, feugiat.
               </Col>
-              <Col lg={3} md={3} sm={3} xs={3} className='center' style={{height:'100%',fontSize:15}}>
-                  Up and Down
+              <Col lg={1} md={1} sm={1} xs={1} className='center' style={{height:'100%',fontSize:30}}>
+                        <RadioGroup name="rating" onChange={this.handleChange}>
+                            <Radio value="up" />Agree
+                            <Radio value="down" />Disagree
+                        </RadioGroup>
               </Col>
           </div>
         )
@@ -43,11 +46,8 @@ class StudentListing extends Component {
                     <Col lg={7} md={7} sm={7} xs={7} className='center' style={{height:'100%',fontSize:30}}>
                         <p style={{width:'100%'}}>ECSE 428:</p>
                     </Col>
-                    <Col lg={1} md={1} sm={1} xs={1} className='center' style={{height:'100%',fontSize:30}}>
-                        <RadioGroup name="rating" onChange={this.handleChange}>
-                            <Radio value="up" />Agree
-                            <Radio value="down" />Disagree
-                        </RadioGroup>
+                    <Col lg={5} md={5} sm={5} xs={5} className='center' style={{height:'100%',fontSize:20}}>
+                        <p style={{width:'100%'}}>Student Feedback</p>
                     </Col>
                 </div>
                 {this.state.showPanel?
