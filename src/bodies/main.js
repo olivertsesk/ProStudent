1d20455edf54e446d29f6a076eea6982e411aff1
durham-abric <durham.abric@mail.mcgill.ca>
2018-02-15 22:28:57 -0500

    Updated Login UI [to match user stories]

--- src/bodies/main.js
+++ src/bodies/main.js
@@ -40,13 +40,13 @@ class Main extends Component {
 
     handleButton(i){
         switch(i){
-            case 1:
+            case 2:
                 this.props.history.push('/student/')
                 break;
-            case 2:
+            case 3:
                 this.props.history.push('/prof/')
                 break;
-            case 3:
+            case 4:
                 this.props.history.push('/admin/')
                 break;
         }
@@ -56,7 +56,7 @@ class Main extends Component {
     main(){
         return(
             <div style={{zIndex:1,width:this.state.width,padding:10}}>
-                <div style={{height:this.state.height/3,display:'flex',flexDirection:'column',justifyContent:'flex-end'}}>
+                <div style={{height:this.state.height/3,justifyContent:'center'}}>
                     <h1 style={{fontSize:this.state.width>1000?this.state.width/20:50,fontWeight:900,color:"#47525e"}}>ProStudent</h1>
                     <h2 style={{fontSize:this.state.width>1000?this.state.width/80:18,fontWeight:600,fontStyle:'italic',color:"#47525e",padding:0,margin:0,lineHeight:1.3}}>
                         Direct communication between a class & its Prof!
@@ -80,15 +80,8 @@ class Main extends Component {
                         <p style={{color:'white'}}>Admin</p>
                     </Button>
                 </ButtonGroup>
-
                 <br/>
                 <br/>
-                <b>Already have an account?</b>
-                <br/>
-                <br/>
-                <Button bsSize="large" style={{background:"rgba(255,255,255,0.3)",border:'none',width:this.state.width>1000?this.state.width/7:this.state.width/3}} onClick={()=>this.setState({mode:1})}>
-                    <b style={{color:'#3d99d4',outline:'none',textShadow:'none'}}>Sign In</b>
-                </Button>
             </div>
         )
     }
@@ -98,100 +91,110 @@ class Main extends Component {
         //mode 2 create account as student
         //mode 3 create account as professor,
         var title = null;
+        var topInput = null;
+        var bottomInput = null;
         switch(mode){
-            case 1:
-                title = "Sign into your account"
-                break;
             case 2:
-                title = "Create a Student account"
+                title = "Login To Class Portal"
+                topInput = "Access Code"
+                bottomInput = "Student ID"
                 break;
             case 3:
-                title = "Create a Professor account"
+                title = "Login To Professor Account"
+                topInput = "Email"
+                bottomInput = "Password"
                 break;
             case 4:
-                title = "Admin Login"
+                title = "Login To Administrator Account"
+                topInput = "Username"
+                bottomInput = "Password"
                 break;
             defaul:
                 break;
         }
-        return(
-            <div className="center" style={{flexDirection:'column',width:'100%',height:'100%'}}>
-                <p style={{color:"#3D99d4",fontSize:20}}>{title}</p>
-                <hr style={{color:"#B3b3b3",border:'solid',width:'90%',borderWidth:0.5}}/>
-                <p style={{padding:0,margin:0,lineHeight:1.3,fontSize:20,width:'80%',textAlign:'left'}}>Email</p>
-                <input style={{width:'80%',border:'none',border:'solid',borderWidth:2,color:'black',borderColor:'#B3b3b3',fontSize:20,outline:'none',boxShadow:'none',borderRadius:5,padding:10}}
-                       onChange={(e)=> this.setState({login:{...this.state.login,email:e.target.value}})}
-                       placeholder="example@mail.mcgill.ca"
-                />
-                <br/>
-                <p style={{padding:0,margin:0,lineHeight:1.3,fontSize:20,width:'80%',textAlign:'left'}}>Password</p>
-                <input style={{width:'80%',border:'none',border:'solid',borderWidth:2,color:'black',borderColor:'#B3b3b3',fontSize:20,outline:'none',boxShadow:'none',borderRadius:5,padding:10}}
-                       onChange={(e)=> this.setState({login:{...this.state.login,password:e.target.value}})}
-                />
-                <br/>
-                {mode==4?
-                    <div style={{width:'80%'}}>
-                        <p style={{padding:0,margin:0,lineHeight:1.3,fontSize:20,width:'100%',textAlign:'left'}}>Employee No</p>
-                        <input style={{width:'100%',border:'none',border:'solid',borderWidth:2,color:'black',borderColor:'#B3b3b3',fontSize:20,outline:'none',boxShadow:'none',borderRadius:5,padding:10}}
-                               onChange={(e)=> this.setState({login:{...this.state.login,password:e.target.value}})}
-                        />
-                        <br/>
-                    </div>
-                    :null
-                }
 
+            return(
+                <div className="center" style={{flexDirection:'column',width:'100%',height:'100%'}}>
+                    <p style={{color:"#3D99d4",fontSize:25}}>{title}</p>
+                    <hr style={{color:"#B3b3b3",border:'solid',width:'90%',borderWidth:0.5}}/>
+                    <p style={{padding:0,margin:0,lineHeight:1.3,fontSize:20,width:'80%',textAlign:'left'}}>{topInput}</p>
+                    <input style={{width:'80%',border:'none',border:'solid',borderWidth:2,color:'black',borderColor:'#B3b3b3',fontSize:20,outline:'none',boxShadow:'none',borderRadius:5,padding:10}}
+                       onChange={(e)=> this.setState({login:{...this.state.login,email:e.target.value}})}
+                    />
+                    <br/>
+                    <p style={{padding:0,margin:0,lineHeight:1.3,fontSize:20,width:'80%',textAlign:'left'}}>{bottomInput}</p>
+                    <input style={{width:'80%',border:'none',border:'solid',borderWidth:2,color:'black',borderColor:'#B3b3b3',fontSize:20,outline:'none',boxShadow:'none',borderRadius:5,padding:10}}
+                       onChange={(e)=> this.setState({login:{...this.state.login,password:e.target.value}})}
+                    />
+                    <br/>
                 <br/>
                 <ButtonGroup>
-                    <Button bsSize="large" style={{background:"#3d99d4",width:this.state.width>1000?this.state.width/7:this.state.width/3}}>
-                        <p style={{color:'white'}}>Submit</p>
+                    <Button bsSize="large" style={{background:"#3d99d4",width:this.state.width>1000?this.state.width/7:this.state.width/3}} onClick={()=>this.handleButton(this.state.mode)}>
+                        <p style={{color:'white'}}>Login</p>
                     </Button>
                     <Button bsSize="large" style={{background:"#3d99d4",width:this.state.width>1000?this.state.width/7:this.state.width/3}} onClick={()=>this.setState({mode:0,login:{...this.state.login,password:null}})}>
                         <p style={{color:'white'}}>Cancel</p>
                     </Button>
                 </ButtonGroup>
-                {
-                    mode==1?
-                        <div>
-                            <p>Temp Navigation Button</p>
-                            <ButtonGroup>
-                                <Button bsSize="large" style={{background:"#3d99d4",width:this.state.width>1000?this.state.width/7:this.state.width/3.3}} onClick={()=>this.handleButton(1)}>
-                                    <p style={{color:'white'}}>Student</p>
-                                </Button>
-                                <Button bsSize="large" style={{background:"#3d99d4",width:this.state.width>1000?this.state.width/7:this.state.width/3.3}} onClick={()=>this.handleButton(2)}>
-                                    <p style={{color:'white'}}>Professor</p>
-                                </Button>
-                                <Button bsSize="large" style={{background:"#3d99d4",width:this.state.width>1000?this.state.width/7:this.state.width/3.3}} onClick={()=>this.handleButton(3)}>
-                                    <p style={{color:'white'}}>Admin</p>
-                                </Button>
-                            </ButtonGroup>
-                        </div>
-                        :null
-                }
-
             </div>
-        )
+            )
     }
 
     render() {
-        return (
-            <div className="App">
-                {
-                    //mode 1 login
-                    //mode 2 create account as student
-                    //mode 3 create account as professor,
-                    (this.state.mode===1||this.state.mode===2||this.state.mode===3||this.state.mode===4)?
+        if(this.state.mode==2||this.state.mode===4){
+            return (
+                <div className="App">
+                        <Panel style={{position:'absolute',
+                            top:this.state.height/8,
+                            left:this.state.width>1000?this.state.width/3:this.state.width/8,
+                            width:this.state.width>1000?this.state.width/3:this.state.width*6/8,
+                            height:this.state.width>1000?this.state.height*3/4:this.state.height*6/8
+                        }}>
+                            {this.panel(this.state.mode)}
+                        </Panel>
+                </div>)
+        }else if (this.state.mode === 3){
+            return(
+                <div className="App">
                     <Panel style={{position:'absolute',
                         top:this.state.height/8,
-                        left:this.state.width>1000?this.state.width/3:this.state.width/8,
+                        left:this.state.width>1000?3*this.state.width/20:this.state.width/8,
                         width:this.state.width>1000?this.state.width/3:this.state.width*6/8,
                         height:this.state.width>1000?this.state.height*3/4:this.state.height*6/8
                     }}>
                         {this.panel(this.state.mode)}
                     </Panel>
-                    :this.main()
-                }
-            </div>
-        );
+                    <Panel style={{position:'absolute',
+                        top:this.state.height/8,
+                        left:this.state.width>1000?13*this.state.width/24:6*this.state.width/8,
+                        width:this.state.width>1000?this.state.width/3:this.state.width*6/8,
+                        height:this.state.width>1000?this.state.height*3/4:this.state.height*6/8
+                    }}>
+                    <div className="center" style={{flexDirection:'column',width:'100%',height:'100%'}}>
+                        <p style={{color:"#3D99d4",fontSize:25}}>Create Professor Account</p>
+                        <hr style={{color:"#B3b3b3",border:'solid',width:'90%',borderWidth:0.5}}/>
+                        <p style={{padding:0,margin:0,lineHeight:1.3,fontSize:20,width:'80%',textAlign:'left'}}>Email</p>
+                        <input style={{width:'80%',border:'none',border:'solid',borderWidth:2,color:'black',borderColor:'#B3b3b3',fontSize:20,outline:'none',boxShadow:'none',borderRadius:5,padding:10}}
+                            onChange={(e)=> this.setState({login:{...this.state.login,email:e.target.value}})}
+                        />
+                        <br/>
+                        <p style={{padding:0,margin:0,lineHeight:1.3,fontSize:20,width:'80%',textAlign:'left'}}>Password</p>
+                        <input style={{width:'80%',border:'none',border:'solid',borderWidth:2,color:'black',borderColor:'#B3b3b3',fontSize:20,outline:'none',boxShadow:'none',borderRadius:5,padding:10}}
+                           onChange={(e)=> this.setState({login:{...this.state.login,password:e.target.value}})}
+                        />
+                        <br/>
+                        <ButtonGroup>
+                            <Button bsSize="large" style={{background:"#3d99d4",width:this.state.width>1000?this.state.width/7:this.state.width/3}}>
+                                <p style={{color:'white'}}>Create Account</p>
+                            </Button>
+                        </ButtonGroup>
+                    </div>
+                    </Panel>
+                </div>
+                )
+        }else{
+            return(this.main())
+        }
     }
 }
 export default Main
