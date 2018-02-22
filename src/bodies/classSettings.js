24f17ced22905e83af1ef3c11352d46fc3681d84
durham-abric <durham.abric@mail.mcgill.ca>
2018-02-21 22:24:34 -0500

    Updated panel sizes to fit contents correctly

--- src/bodies/classSettings.js
+++ src/bodies/classSettings.js
@@ -142,7 +142,7 @@ class ClassSettings extends Component {
                         top:this.state.height/8,
                         left:this.state.width>1000?this.state.width/3:this.state.width/8,
                         width:this.state.width>1000?this.state.width/3:this.state.width*6/8,
-                        height:this.state.width>1000?this.state.height*3/4:this.state.height*6/8
+                        height:this.state.width>1000?this.state.height*7/8:this.state.height*13/16
                     }}>
                         {this.panel(this.state.mode)}
                     </Panel>
