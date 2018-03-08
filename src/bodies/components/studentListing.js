bc0877fbdfcb8a6c4186381f67a67aed58cb3a12
Jeremy <jgomallette@gmail.com>
2018-03-08 09:40:03 -0500

    Code cleanup and Abstract CSS from Student Landing page

--- src/bodies/components/studentListing.js
+++ src/bodies/components/studentListing.js
@@ -2,150 +2,145 @@
  * Created by jaewonlee on 2018. 2. 10..
  */
 import React, { Component } from 'react';
-import{
-    Navbar,
-    Nav,
-    NavItem,
-    Col,
-    Button,
-    Radio
-} from 'react-bootstrap'
+import { Col, Button } from 'react-bootstrap';
+
 import * as firebase from 'firebase'
 import * as data from './../../data'
 
 class StudentListing extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showPanel:false,
-            comments : []
-        };
-        this.comment = this.comment.bind(this)
-        this.upvote = this.upvote.bind(this)
-        this.downvote = this.downvote.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPanel:false,
+      comments : []
+    };
+
+    this.comment = this.comment.bind(this)
+    this.upvote = this.upvote.bind(this)
+    this.downvote = this.downvote.bind(this)
+  }
+
+  getRating(item){
+    var rating = 0;
+    if (item.val().up && item.val().down) {
+      rating = item.val().up.list.length - item.val().down.list.length;
+    } else if (item.val().up) {
+      rating = item.val().up.list.length;
+    } else if (item.val().down) {
+      rating = -item.val().down.list.length;
     }
 
-    getRating(item){
-        var rating = 0;
-        if (item.val().up && item.val().down){
-            rating = item.val().up.list.length - item.val().down.list.length;
-        }else if(item.val().up){
-            rating = item.val().up.list.length;
-        }else if(item.val().down){
-            rating = -item.val().down.list.length;
+    return(rating);
+  }
+
+  componentWillMount(){
+    firebase.database().ref('/classes/'+data.getCourseID()+'/feedback').on('value',(snapshot) =>{
+      var comments = this.state.comments;
+      comments =[];
+
+      snapshot.forEach(function(item) {
+        comments.push(item);
+      });
+
+      //sort listing by number of votes
+      comments.sort((a,b)=>{
+        return (this.getRating(b)-this.getRating(a));
+      });
+
+      this.setState({comments});
+    })
+
+    // console.log(this.props)
+  }
+
+  upvote(item){
+    var up_list = [];
+    if (item.val().up) {
+      up_list= item.val().up.list;
+    }
+
+    up_list.push(data.getID());
+    firebase.database().ref('/classes/'+data.getCourseID()+'/feedback/'+item.key + '/up').update({
+      list:up_list
+    }).then(()=>{
+    }).catch(()=>{
+      alert("Error in upvoting comment")
+    })
+  }
+
+  downvote(item) {
+    var down_list = [];
+    if(item.val().down) {
+      down_list= item.val().down.list;
+    }
+
+    down_list.push(data.getID());
+    firebase.database().ref('/classes/'+data.getCourseID()+'/feedback/'+item.key + '/down').update({
+      list:down_list
+    }).then(()=>{
+    }).catch(()=>{
+      alert("Error in downvoting comment")
+    })
+  }
+
+  isActive(i){
+    var myID = data.getID();
+    var up = this.state.comments[i].val().up;
+    var down = this.state.comments[i].val().down;
+    if (this.state.comments[i].val().up && this.state.comments[i].val().down) {
+      return(!up.list.includes(myID)&&!down.list.includes(myID));
+    } else if (this.state.comments[i].val().up) {
+      return(!up.list.includes(myID));
+    } else if (this.state.comments[i].val().down) {
+      return(!down.list.includes(myID));
+    } else {
+      return(true);
+    }
+  }
+
+  comment(item,i){
+    return(
+      <div className="bottomsep commentWrapper" key ={i}>
+        <Col lg={2} md={2} sm={2} xs={2} className="center commentCol">
+          <p className="center commentType">{item.val().tag?item.val().tag:"No Tag"}</p>
+        </Col>
+        <Col lg={6} md={6} sm={6} xs={6} className="center commentCol">
+          <p className="center comment">{item.val().comment}</p>
+        </Col>
+        <Col lg={2} md={2} sm={2} xs={2} className="center commentCol">
+          <Button className="ratingButton" onClick={()=>this.upvote(item)}
+            active={this.isActive(i)}
+            disabled={!this.isActive(i)}>
+            <img src={require('./../../image/thumbs_up.png')} alt="/\\" className="ratingImg"/>
+          </Button>
+          <Button className="ratingButton" onClick={()=>this.downvote(item)}
+            active={this.isActive(i)}
+            disabled={!this.isActive(i)}>
+            <img src={require('./../../image/thumbs_down.png')} alt="\\/" className="ratingImg"/>
+          </Button>
+        </Col>
+        <Col lg={2} md={2} sm={2} xs={2} className="center rating">
+          <p>Rating: {this.getRating(item)}</p>
+        </Col>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="listing" onClick={()=>this.setState({showPanel:!this.state.showPanel})}>
+          <p className="listingProf">{this.props.item.course.code}</p>
+          <p className="listingTitle">{" "+this.props.item.course.title}</p>
+        </div>
+        <div>
+        {
+          this.state.comments.map((item,i)=>this.comment(item,i))
         }
-
-        return(rating);
-    }
-
-    componentWillMount(){
-        firebase.database().ref('/classes/'+data.getCourseID()+'/feedback').on('value',(snapshot) =>{
-            var comments = this.state.comments;
-            comments =[];
-            snapshot.forEach(function(item) {
-                comments.push(item);
-            });
-             //sort listing by number of votes
-            comments.sort((a,b)=>{
-                    return (this.getRating(b)-this.getRating(a));
-            });
-            this.setState({comments})
-        })
-        console.log(this.props)
-    }
-
-    upvote(item){
-        var up_list = [];
-        if(item.val().up){
-            up_list= item.val().up.list;
-        }
-        up_list.push(data.getID());
-        firebase.database().ref('/classes/'+data.getCourseID()+'/feedback/'+item.key + '/up').update({
-            list:up_list
-        }).then(()=>{
-        }).catch(()=>{
-            alert("Error in upvoting comment")
-        })
-    }
-
-    downvote(item){
-        var down_list = [];
-        if(item.val().down){
-            down_list= item.val().down.list;
-        }
-        down_list.push(data.getID());
-        firebase.database().ref('/classes/'+data.getCourseID()+'/feedback/'+item.key + '/down').update({
-            list:down_list
-        }).then(()=>{
-        }).catch(()=>{
-            alert("Error in downvoting comment")
-        })
-    }
-
-    isActive(i){
-        var myID = data.getID();
-        var up = this.state.comments[i].val().up;
-        var down = this.state.comments[i].val().down;
-        if (this.state.comments[i].val().up && this.state.comments[i].val().down){
-            return(!up.list.includes(myID)&&!down.list.includes(myID));
-        }else if(this.state.comments[i].val().up){
-            return(!up.list.includes(myID));
-        }else if(this.state.comments[i].val().down){
-            return(!down.list.includes(myID));
-        }else{
-            return(true);
-        }
-    }
-
-    comment(item,i){
-        return(
-            <div style={{height:100,border:"solid",borderColor:'#343f4b'}} key ={i}>
-                <Col lg={2} md={2} sm={2} xs={2} className='center' style={{height:'100%',fontSize:15}}>
-                    <p>{item.val().tag?item.val().tag:"No Tag"}</p>
-                </Col>
-                <Col lg={6} md={6} sm={6} xs={6} className='center' style={{height:'100%',fontSize:15}}>
-                    <p>{item.val().comment}</p>
-                </Col>
-                <Col lg={2} md={2} sm={2} xs={2} className='center' style={{height:'100%',fontSize:30,color:'#343f4b'}}>
-                    <Button onClick={()=>this.upvote(item)}
-                            active={this.isActive(i)}
-                            disabled={!this.isActive(i)}
-                    >
-                        <img src={require('./../../image/thumbs_up.png')} style={{width:40,height:50}}/>
-                    </Button>
-                    <Button onClick={()=>this.downvote(item)}
-                            active={this.isActive(i)}
-                            disabled={!this.isActive(i)}
-                    >
-                        <img src={require('./../../image/thumbs_down.png')} style={{width:40,height:50}}/>
-                    </Button>
-                </Col>
-                <Col lg={2} md={2} sm={2} xs={2} className='center' style={{height:'100%',fontSize:25,color:'#343f4b'}}>
-                    <p>Rating: {this.getRating(item)}</p>
-                </Col>
-            </div>
-        )
-    }
-
-    render() {
-        return (
-            <div>
-                <div style={{height:150,border:"solid",borderColor:'#343f4b'}} onClick={()=>this.setState({showPanel:!this.state.showPanel})}>
-                    <Col lg={3} md={3} sm={3} xs={3} className='center' style={{height:'100%',fontSize:30}}>
-                        <p style={{width:'100%'}}>{this.props.item.course.code}</p>
-                    </Col>
-                    <Col lg={9} md={9} sm={9} xs={9} className='center' style={{height:'100%',fontSize:45}}>
-                        <p style={{width:'100%'}}>{" "+this.props.item.course.title}</p>
-                    </Col>
-                </div>
-                    <div>
-                        {
-                            this.state.comments.map((item,i)=>this.comment(item,i))
-                        }
-                    </div>
-            </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default StudentListing;
--- src/bodies/studentLanding.js
+++ src/bodies/studentLanding.js
@@ -3,16 +3,14 @@
  */
 
 import React, { Component } from 'react';
-import{
-    Col,
-    ButtonGroup,
-    Button,
-    DropdownButton,
-    MenuItem
-} from 'react-bootstrap'
+import { Redirect } from 'react-router-dom';
 import {
-    Redirect
-} from 'react-router-dom';
+  Col,
+  ButtonGroup,
+  Button,
+  DropdownButton,
+  MenuItem
+} from 'react-bootstrap';
 
 import CommentList from "./components/studentListing"
 import * as data from './../data'
@@ -20,95 +18,94 @@ import * as firebase from 'firebase'
 
 
 class StudentLanding extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading:true,
-            height:window.innerHeight,
-            width:window.innerWidth,
-            studentID:null,
-            courseInfo:null,
-            currentTag:"Other",
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading:true,
+      height:window.innerHeight,
+      width:window.innerWidth,
+      studentID:null,
+      courseInfo:null,
+      currentTag:"Other",
+    };
+  }
+
+  sendFeedback() {
+    // var comment = this.state.feedback.comment
+    firebase.database().ref('/classes/' + data.getCourseID() + '/feedback').push({
+      comment:this.state.feedback.comment,studentID:this.state.studentID,tag:this.state.currentTag
+    }).then(()=>{
+      this.setState({mode:0})
+    }).catch((e)=>{
+      alert(e);
+    })
+  }
+
+  resize = () => {
+    this.setState({width:window.innerWidth,height:window.innerHeight});
+  }
+
+  componentWillMount(){
+    this.setState({studentID:data.getID(),courseInfo:data.getInfo()});
+    this.setState({loading:false});
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.resize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize);
+  }
+
+  setTag(tag){
+    this.setState({currentTag:tag});
+  }
+
+  render() {
+    if(!this.state.courseInfo) {
+      return(<Redirect to={'/main'}/>)
     }
 
-    sendFeedback() {
-        // var comment = this.state.feedback.comment
-        firebase.database().ref('/classes/' + data.getCourseID() + '/feedback').push({
-            comment:this.state.feedback.comment,studentID:this.state.studentID,tag:this.state.currentTag
-        }).then(()=>{
-            this.setState({mode:0})
-        }).catch((e)=>{
-            alert(e);
-        })
-    }
-
-    resize = () => {
-        this.setState({width:window.innerWidth,height:window.innerHeight})
-    }
-
-    componentWillMount(){
-        this.setState({studentID:data.getID(),courseInfo:data.getInfo()})
-        this.setState({loading:false})
-    }
-
-    componentDidMount() {
-        window.addEventListener('resize', this.resize)
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.resize)
-    }
-
-    setTag(tag){
-        this.setState({currentTag:tag});
-    }
-
-    render() {
-        if(!this.state.courseInfo){
-            return(
-                <Redirect to={'/main'}/>
-            )
-        }
-        return (
-            <div style={{width:'100%',height:this.state.height-65,padding:80}}>
-                <Col lg={12} md={12} sm={12} xs={12} style={{height:'100%',padding:0,background:'white',overflowY:"scroll"}}>
-                    <div style={{width:'100%',padding:10}}>
-                        <h2>Leave your own feedback:</h2>
-                        <div style={{display:'flex',flexDirection:'row'}}>
-                            <DropdownButton title={this.state.currentTag} id="dropdown-size-large" style={{width:'100%',height:'100%'}}>
-                                <MenuItem eventKey="1" value="Question" onSelect={()=>this.setTag("Question")}>Question</MenuItem>
-                                <MenuItem divider />
-                                <MenuItem eventKey="2" value="Suggestion" onSelect={()=>this.setTag("Suggestion")}>Suggestion</MenuItem>
-                                <MenuItem divider />
-                                <MenuItem eventKey="3" value="Compliment" onSelect={()=>this.setTag("Compliment")}>Compliment</MenuItem>
-                                <MenuItem divider />
-                                <MenuItem eventKey="4" value="Complaint" onSelect={()=>this.setTag("Complaint")}>Complaint</MenuItem>
-                                <MenuItem divider />
-                                <MenuItem eventKey="5" value="Other" onSelect={()=>this.setTag("Other")}>Other</MenuItem>
-                            </DropdownButton>
-                            <input style={{width:'100%',border:'none',border:'solid',borderWidth:2,color:'black',borderColor:'#B3b3b3',fontSize:20,outline:'none',boxShadow:'none',borderRadius:5,padding:10}}
-                                   placeholder="Comment here..."
-                                   onChange={(e)=> this.setState({feedback:{...this.state.feedback,comment:e.target.value}})}
-                                   />
-                        </div>
-                        <div style={{display:'flex',flexDirection:'row',justifyContent:'flex-end'}}>
-                            <ButtonGroup class="center">
-                                <Button bsSize="large" style={{background:"#3d99d4",width:5*this.state.width/6}} onClick={()=>{
-                                    this.sendFeedback();
-                                    }}>
-                                    <p style={{color:'white', fontSize:20}}>Submit Feedback</p>
-                                </Button>
-                            </ButtonGroup>
-                        </div>
-                    </div>
-                    <div>
-                        <CommentList item={this.state.courseInfo}/>
-                    </div>
-                </Col>
+    return (
+      <div className="landingPage" style={{height:this.state.height-100}}>
+        <Col lg={12} md={12} sm={12} xs={12} className="scroll feedbackwrapper">
+          <div className="feedbackInnerWrapper">
+            <h2 className="title">Leave Your Feedback</h2>
+            <div className="feedbackline">
+              <DropdownButton title={this.state.currentTag} id="dropdown-size-large" className="formselector">
+                <MenuItem eventKey="1" value="Question" onSelect={()=>this.setTag("Question")}>Question</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="2" value="Suggestion" onSelect={()=>this.setTag("Suggestion")}>Suggestion</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="3" value="Compliment" onSelect={()=>this.setTag("Compliment")}>Compliment</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="4" value="Complaint" onSelect={()=>this.setTag("Complaint")}>Complaint</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="5" value="Other" onSelect={()=>this.setTag("Other")}>Other</MenuItem>
+              </DropdownButton>
+              <input className="forminput" placeholder="Comment here..." onChange={(e)=> this.setState({feedback: {...this.state.feedback, comment: e.target.value}})}/>
             </div>
-        )
-    }
+            <div className="feedbackline">
+            <ButtonGroup className="center">
+              <Button bsSize="large" className="loginbutton" style={{width:this.state.width/3}} onClick={()=>{
+                this.sendFeedback();
+                }}>
+                <p style={{color:'white', fontSize:20}}>Submit Feedback</p>
+              </Button>
+            </ButtonGroup>
+          </div>
+
+          <hr className="formseperator"/>
+
+        </div>
+        <div>
+          <CommentList item={this.state.courseInfo}/>
+        </div>
+      </Col>
+    </div>
+  )
+}
 }
 
 export default StudentLanding;
